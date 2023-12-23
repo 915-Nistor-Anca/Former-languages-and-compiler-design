618a1b1200ceb285e82043994c0d2ea85ebff0ea
Anca Alexia Nistor <ancanistor16@gmail.com>
2023-12-23 12:48:31 +0200

    Revert "lab7 with ll(1) and table implementation"
    This reverts commit bc8e7a3b0d5df2a85835e7a32ee714ab4e72bf1b.

--- Lab6/main.py
+++ Lab6/main.py
@@ -1,15 +1,9 @@
-from tabulate import tabulate
-
 from Grammar import Grammar
-from ParserOutput import ParserOutput
 
 if __name__ == '__main__':
-    g = Grammar("g6.txt")
+    g = Grammar("g7.txt")
     g.readFromFile()
     print(g.__str__())
     print(g.checkIfCFG())
     print(g.firstSets())
     print(g.followSets())
-    print(tabulate(g.constructParsingTable(), headers=[" "]+ g.terminals + ['$'], tablefmt="fancy_grid"))
-    po = ParserOutput(g)
-    po.parse("a * ( a + a )")
